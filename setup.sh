03bc4e225302e207b2b81434addca373f3925d16
Vladimir Alekseichenko <vova@vova.me>
2017-12-29 00:43:43 +0100

    add setup script

---
+++ setup.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install -y cmake g++ git htop pkg-config libopenblas-dev libopencv-dev libboost-all-dev
+
+sudo chown -R `whoami`:`whoami` /opt
+cd /opt
+
+echo "bb2656314d22aecae6af243ddbbfb32c Anaconda3-5.0.0-Linux-x86_64.sh" | md5sum -c && exit
+bash Anaconda3-5.0.0-Linux-x86_64.sh
+
+source ~/.bashrc
+
+#enter x3
+
+### XGBoost
+cd /opt
+git clone --recursive https://github.com/dmlc/xgboost; cd xgboost
+make -j4
+cd python-package
+python setup.py install
+
+### LightGBM
+cd /opt
+git clone --recursive https://github.com/Microsoft/LightGBM ; cd LightGBM
+mkdir build ; cd build
+cmake ..
+make -j4
+cd ../python-package
+python setup.py install
+
+### Catboost
+pip --no-cache-dir install https://github.com/catboost/catboost/releases/download/v0.5.2/catboost-0.5.2-cp36-none-manylinux1_x86_64.whl
+
+
+### MxNet
+#cd /opt
+#git clone --recursive https://github.com/apache/incubator-mxnet.git mxnet --branch 0.12.0; cd mxnet
+#make -j $(nproc) USE_OPENCV=1 USE_BLAS=openblas
+#python setup.py install
+
+
+### tensorflow & keras
+#conda install -c conda-forge -y tensorflow 
+pip --no-cache-dir install https://github.com/mind/wheels/releases/download/tf1.4-cpu/tensorflow-1.4.0-cp36-cp36m-linux_x86_64.whl
+conda install -c conda-forge -y keras==2.0.9
+
+### others
+pip install hyperopt==0.1
+pip install xgbfir==0.3.1
+pip install graphviz==0.8.1
+
+
+### dlib
+cd /opt
+git clone https://github.com/davisking/dlib ; cd dlib
+#mkdir build; cd build; cmake .. -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
+#cd ..
+python setup.py install --yes USE_AVX_INSTRUCTIONS
+
+
+pip install pandas_profiling==1.4.0
+pip install mpld3==0.3
+
+
+
+
+
+
+
+
